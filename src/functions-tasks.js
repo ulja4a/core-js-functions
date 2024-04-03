d02f93740fbe289a18ddbf050121a3189a0eab21
ulja4a <ulja4a@gmail.com>
2024-04-03 17:45:54 +0300

    feat: 2 tests passed return body and return arguments

--- src/functions-tasks.js
+++ src/functions-tasks.js
@@ -32,8 +32,11 @@ function getCurrentFunctionName() {
  *   getFunctionBody(hiHello) => "function hiHello() { console.log('hello world'); }"
  *
  */
-function getFunctionBody(/* func */) {
-  throw new Error('Not implemented');
+function getFunctionBody(func) {
+  if (func === undefined) {
+    return '';
+  }
+  return func.toString();
 }
 
 /**
@@ -50,8 +53,8 @@ function getFunctionBody(/* func */) {
  *  ]) => [0, 1, 2]
  *
  */
-function getArgumentsCount(/* funcs */) {
-  throw new Error('Not implemented');
+function getArgumentsCount(funcs) {
+  return funcs.map((fn) => fn.length);
 }
 
 /**
